2e1fe761d9a2f7ebcdd76944a2009606154e5dca
Latricia Nickelberry <tricianickelberry@gmail.com>
2019-12-14 15:30:20 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,11 @@
 def oxford_comma(array)
-"fiddleheads, okra, kohlrabi".split (",")  
-
+  fiddleheads, okra, kohlrabi. split (",")
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    array[-1].insert(0, "and ")
+  end
+  array.join(", ")    
 end
 
-puts oxford_comma
+puts oxford_comma(array)
