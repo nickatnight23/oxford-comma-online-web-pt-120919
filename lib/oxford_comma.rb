1062a79b0952cfe4d9700efc540d9a1d6f6f3346
Latricia Nickelberry <tricianickelberry@gmail.com>
2019-12-14 15:26:24 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,8 @@
 def oxford_comma(array)
-"fiddleheads, okra, kohlrabi".split (",")  
-
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    array[-1].insert(0, "and ")
+  end
+  array.join(", ")    
 end
-
-puts oxford_comma
